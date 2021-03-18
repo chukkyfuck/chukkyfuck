66e49535437680705c211ebb7720950d45671a9c
G0oDkat <torben.pietreck@rwth-aachen.de>
2021-03-18 19:33:29 +0100

    masonry

---
+++ js/masonry.js
@@ -0,0 +1,13 @@
+﻿$(function() {
+    var $masonry = $("#masonry");
+
+    $masonry.imagesLoaded(function() {
+        $masonry.masonry(
+            {
+                columnWidth: ".masonry-sizer",
+                itemSelector: ".masonry-item",
+                percentPosition: true,
+                transitionDuration: 0
+            });
+    });
+});
