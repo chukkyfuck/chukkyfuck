c807b2f871e08e7961e0238d193fe574a60f768c
G0oDkat <torben.pietreck@rwth-aachen.de>
2021-06-20 14:25:06 +0200

    lazy image loading

--- js/gallery.js
+++ js/gallery.js
@@ -4,50 +4,47 @@
     var $photoSwipe = $(".pswp");
     var gallery;
     
-    $masonry.imagesLoaded(function()
-    {
-        $masonry.masonry(
-            {
-                columnWidth: ".masonry-sizer",
-                itemSelector: ".masonry-item",
-                percentPosition: true,
-                transitionDuration: 0
-            });
-
-        var items = $masonry.find(".figure").map(function(index, figure)
+    $masonry.masonry(
         {
-            var $figure = $(figure);
-            var $image = $figure.find("img");
-            //var $caption = $figure.find(".figure-caption");
+            columnWidth: ".masonry-sizer",
+            itemSelector: ".masonry-item",
+            percentPosition: true,
+            transitionDuration: 0
+        });
 
-            $figure.click(function()
+    var items = $masonry.find(".figure").map(function(index, figure)
+    {
+        var $figure = $(figure);
+        var $image = $figure.find("img");
+        //var $caption = $figure.find(".figure-caption");
+
+        $figure.click(function()
+        {
+            var options =
             {
-                var options =
+                index: index,
+                shareEl: false,
+                showHideOpacity: true,
+                getThumbBoundsFn: function (index)
                 {
-                    index: index,
-                    shareEl: false,
-                    showHideOpacity: true,
-                    getThumbBoundsFn: function (index)
-                    {
-                        var image = items[index].image;
-                        var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
-                        var rect = image.getBoundingClientRect();
+                    var image = items[index].image;
+                    var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+                    var rect = image.getBoundingClientRect();
 
-                        return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
-                    }
-                };
+                    return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+                }
+            };
 
-                gallery = new PhotoSwipe($photoSwipe[0], PhotoSwipeUI_Default, items, options);
-                gallery.init();
-            });
+            gallery = new PhotoSwipe($photoSwipe[0], PhotoSwipeUI_Default, items, options);
+            gallery.init();
+        });
 
-            return {
-                src: $image.attr("src"),
-                w: $image[0].naturalWidth,
-                h: $image[0].naturalHeight,
-                //title: $caption.html(),
-                image: $image[0]
+        return {
+            src: $image.attr("src"),
+            w: $image[0].naturalWidth,
+            h: $image[0].naturalHeight,
+            //title: $caption.html(),
+            image: $image[0]
         };
-        }).get();
-    });
+    }).get();
 });
